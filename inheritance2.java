e85524a9ea12b1d8024a0563f53f96a07a86d006
rohitsingla123 <99662624+rohitsingla123@users.noreply.github.com>
2023-11-03 06:54:54 +0530

    Add files via upload

---
+++ inheritance2.java
@@ -0,0 +1,24 @@
+/*Write a Java program to create a class called Vehicle with a method called drive(). Create a subclass called Car that overrides the drive() method to print
+ "Repairing a car".*/
+
+
+class Vehicle
+{
+
+public void drive()
+{
+System.out.println("I am driving");
+}
+
+}
+
+class Car extends Vehicle
+{
+public void drive()
+{
+System.out.println("Repairing a car");
+} 
+
+
+
+}
